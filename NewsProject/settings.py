fa81138a7fd9143d80e8a9f81226e408a8b2a612
Praddyumn Yadav <praddyumny@gmail.com>
2022-10-24 16:55:06 +0530

    Try to solve StaticFiles Errore

--- NewsProject/settings.py
+++ NewsProject/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+DISABLE_COLLECTSTATIC=1
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
