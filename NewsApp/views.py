0b057018d4864446a3ee7a806318a67afb9a68bc
Praddyumn Yadav <praddyumny@gmail.com>
2022-10-26 12:51:17 +0530

    Added BBC News to our Page and Optimized it a little bit for SEO

--- NewsApp/views.py
+++ NewsApp/views.py
@@ -5,7 +5,7 @@ import requests
 # Create your views here.
 def index(request):
     r = requests.get(
-        'http://api.mediastack.com/v1/news?access_key=e77a1cc74ab7b96f04376b4b4afc6801&countries=us,gb,de,in&sources=en,cnn&categories=business,science,technology,entertainment')
+        'http://api.mediastack.com/v1/news?access_key=e77a1cc74ab7b96f04376b4b4afc6801&countries=in,us,gb,de&sources=en,bbc,cnn&categories=business,science,technology,entertainment')
 
 
     res = r.json()
@@ -19,7 +19,10 @@ def index(request):
     for i in data:
         title.append(i['title'])
         description.append(i['description'])
-        image.append(i['image'])
+        if i['image'] == None:
+            image.append("https://upload.wikimedia.org/wikipedia/en/thumb/f/ff/BBC_News.svg/1200px-BBC_News.svg.png")
+        else:
+            image.append(i['image'])
         url.append(i['url'])
 
     news = zip(title, description, image, url)
